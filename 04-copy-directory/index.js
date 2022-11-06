01c00513d64a666e19e8580ea567f19827dae226
KohnoA <san.kohno@yandex.ru>
2022-11-06 09:11:18 +0300

    feat: completed task 04-copy-directory

--- 04-copy-directory/index.js
+++ 04-copy-directory/index.js
@@ -1,1 +1,30 @@
+const { mkdir, copyFile, readdir, rm } = require('fs/promises')
+const path = require('path')
+const filesCopyPath = path.join(__dirname, 'files-copy')
+const filesPath = path.join(__dirname, 'files')
 
+async function createCopyDirectory() {
+    try {
+        await mkdir(filesCopyPath, {recursive: true})
+        await clearCopyDirectory()
+
+        const files = await readdir(filesPath)
+
+        for(let file of files) {
+            await copyFile(path.join(filesPath, file), path.join(filesCopyPath, file))
+        }
+
+    } catch(err) {
+        console.error(err)
+    }
+}
+
+async function clearCopyDirectory() {
+    const filesInCopyDir = await readdir(filesCopyPath)
+
+    for(let file of filesInCopyDir) {
+        await rm(path.join(filesCopyPath, file), {force: true})
+    }
+}
+
+createCopyDirectory()
